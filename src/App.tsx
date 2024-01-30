dee06137eff00d789c75f77ddad15d3a04aa77c9
Yuto Urashima <yuto.urashima24@gmail.com>
2024-01-30 18:55:15 +0900

    カウント操作のボタンを作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,31 @@
-import { Box, Button, Paper, Typography } from "@mui/material"
-import { useState } from 'react'
+import { Button, Paper, Typography } from "@mui/material"
+import { useCallback, useState } from 'react'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-  const countUp = () => {
+
+  const countUp = useCallback(() => {
     setCount(count + 1)
-  }
+  }, [count])
+
+  const countDown = useCallback(() => {
+    if (count > 0) {
+      setCount(count - 1)
+    }
+  }, [count])
 
   return (
     <Paper sx={{ p: 10 }}>
-      <Box bgcolor={"red"} sx={{ p: 2 }}>
-        <Typography variant="body1" >
-          {count}
-        </Typography>
-        <Button variant="contained" onClick={countUp}>
-          ボタン
-        </Button>
-      </Box>
+      <Typography variant="body1" >
+        {count}
+      </Typography>
+      <Button variant="contained" onClick={countUp}>
+          カウントアップボタン
+      </Button>
+      <Button variant="contained" onClick={countDown}>
+          カウントダウンボタン
+      </Button>
     </Paper>
   )
 }
